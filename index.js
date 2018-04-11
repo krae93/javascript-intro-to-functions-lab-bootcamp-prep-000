e969c327c71d8147e7da135a21572239911350fa
Kelly Rae Ramirez <k.rae1993@yahoo.com>
2018-04-11 19:26:06 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -20,4 +20,6 @@ function sayHiToGrandma(sayHiToGrandma) {
   if (sayHiToGrandma === sayHiToGrandma.toLowerCase())
   return "I can't hear you!"
   console.log("I can't hear you!")
-}
+} 
+if (sayHiToGrandma === sayHiToGrandma.toUpperCase())
+return "YES INDEED!"
