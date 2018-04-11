a13dc95ac48ba8e87e24f0ef06608d36ba7e6cf9
Kelly Rae Ramirez <k.rae1993@yahoo.com>
2018-04-11 19:13:40 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,9 +1,22 @@
 function shout(sayHiToGrandma) {
   return sayHiToGrandma.toUpperCase()
 }
+function logShout(sayHiToGrandma) {
+  var upper = sayHiToGrandma.toUpperCase()
+  console.log(upper)
+}
 function whisper(sayHiToGrandma) {
   return sayHiToGrandma.toLowerCase()
 }
+function logWhisper(sayHiToGrandma) {
+  var lower = sayHiToGrandma.toLowerCase()
+  console.log(lower)
+}
 function sayHiToGrandma() {
   console.log("Hi, Grandma!")
+}
+
+function sayHiToGrandma(sayHiToGrandma) {
+  if (sayHiToGrandma === sayHiToGrandma.toLowerCase())
+  return "I can't hear you!"
 }
